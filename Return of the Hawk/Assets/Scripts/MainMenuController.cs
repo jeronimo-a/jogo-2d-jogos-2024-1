64ab150de7d097232eee5e153f7f97449111627b
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-04-17 12:10:50 -0300

    Scripts básicos dos botões de play e quit do menu princiapl

--- Return of the Hawk/Assets/Scripts/MainMenuController.cs
+++ Return of the Hawk/Assets/Scripts/MainMenuController.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour
-{
-    public void QuitGame() {
+public class MainMenuController : MonoBehaviour {
+
+    public string firstGameScene;
+
+    public void PlayButton() {
+        SceneManager.LoadScene(firstGameScene);
+    }
+
+    public void QuitButton() {
         Application.Quit();
     }
 }
