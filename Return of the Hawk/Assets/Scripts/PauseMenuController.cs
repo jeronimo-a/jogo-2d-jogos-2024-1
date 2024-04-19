7b2b3708a15edc9e38135ec97bcf5d1d04236deb
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-04-19 09:25:30 -0300

    Prefab do menu de pause pronto para ser usado

---
+++ Return of the Hawk/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour {
+
+    public string mainMenuScene;
+    public KeyCode pauseKey = KeyCode.Escape;
+    public bool paused = false;
+    public GameObject mainCanvas;
+
+
+    public void QuitButton() {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    public void ResumeButton() {
+        paused = false;
+        mainCanvas.SetActive(false);
+    }
+
+    void Start() {
+        paused = false;
+        mainCanvas.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(pauseKey)) {
+            paused = true;
+            mainCanvas.SetActive(true);
+        }
+    }
+}
