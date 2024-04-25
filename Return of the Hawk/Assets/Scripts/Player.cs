357fce184846b26a63c6584b6787289001309e75
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2024-04-25 15:45:26 -0300

    Adding health, mouse looking, shooting, reloading, collision and death logic

--- Return of the Hawk/Assets/Scripts/Player.cs
+++ Return of the Hawk/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float health = 100.0f;
 
     private Rigidbody2D rb;
     
@@ -24,9 +25,59 @@ public class Player : MonoBehaviour
         Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized * speed;
         rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
     }
+    
+    void RotateTowardsMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = -direction;
+    }
+    
+    void Shoot()
+    {
+        GameObject bullet = ObjectPool.SharedInstance.GetPooledObject();
+        if (bullet != null)
+        {
+            bullet.transform.position = transform.position;
+            bullet.transform.rotation = transform.rotation;
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.up * 10;
+            bullet.SetActive(true);
+        }
+    }
 
     void FixedUpdate()
     {
         Move();
+        RotateTowardsMouse();
+    }
+
+    void Reload()
+    {
+        ObjectPool.SharedInstance.pooledObjects.ForEach(obj => obj.SetActive(false));;
+    }
+
+    void Update()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+        
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            Shoot();
+        
+        if (Input.GetKeyDown(KeyCode.R))
+            Reload();
+        
+        // ONLY DAMAGE TEST
+        if (Input.GetKeyDown(KeyCode.T))
+            health -= 10;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            health -= 10;
+            collision.gameObject.SetActive(false);
+        }
     }
 }
