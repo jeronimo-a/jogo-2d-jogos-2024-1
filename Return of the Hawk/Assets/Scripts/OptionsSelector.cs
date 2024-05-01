f146a42017d740785879a05339665d54846d3f93
carolineclc <carolineclc@al.insper.edu.br>
2024-05-01 18:54:20 -0300

    adicionando scripts, algumas das salas do terceiro nivel

---
+++ Return of the Hawk/Assets/Scripts/OptionsSelector.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsSelector : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+
+
+}
