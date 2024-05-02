7506694f66372564bd86e1561354ad7a8a113010
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2024-05-02 16:06:59 -0300

    Updating GameManager to deal with player health and ammo

--- Return of the Hawk/Assets/Scripts/GameManager.cs
+++ Return of the Hawk/Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private float playerHealth = 1000.0f;
+    private int playerMagazineAmmo = 12;
+    private int playerReserveAmmo = 24;
+    private float enemyHealth = 20.0f;
     
     void Awake()
     {
@@ -16,8 +19,55 @@ public class GameManager : MonoBehaviour
         return playerHealth;
     }
     
-    public void TakeDamage(float damage)
+    public int GetPlayerMagazineAmmo()
     {
-        playerHealth -= damage;
+        return playerMagazineAmmo;
+    }
+    
+    public int GetPlayerReserveAmmo()
+    {
+        return playerReserveAmmo;
+    }
+    
+    public int GetPlayerTotalAmmo()
+    {
+        return playerMagazineAmmo + playerReserveAmmo;
+    }
+    
+    public float GetEnemyHealth()
+    {
+        return enemyHealth;
+    }
+    
+    public void TakeDamage(float damage, string source)
+    {
+        if (source == "Player")
+        {
+            playerHealth -= damage;
+        } else if (source == "Enemy")
+        {
+            enemyHealth -= damage;
+        }
+    }
+    
+    public void PickUpAmmo(int ammo = 12)
+    {
+        playerReserveAmmo += ammo;
+    }
+    
+    public void Reload()
+    {
+        if (playerReserveAmmo > 0)
+        {
+            int ammoNeeded = 12 - playerMagazineAmmo;
+            if (playerReserveAmmo >= ammoNeeded)
+            {
+                playerReserveAmmo -= ammoNeeded;
+                playerMagazineAmmo = 12;
+                return;
+            }
+            playerMagazineAmmo += playerReserveAmmo;
+            playerReserveAmmo = 0;
+        }
     }
 }
